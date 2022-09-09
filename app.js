e52b216bcb86c0f2deccd09e150e6288f579534b
Sandra Valenzuela <valenzuela.sandrac@gmail.com>
2022-09-08 21:51:27 -0300

    Proyecto Final

--- app.js
+++ app.js
@@ -1,39 +1,52 @@
-
-    class usuario {
-        constructor(nombre) {
-            this.nombre = nombre
-        }
+//Uso del localStorage
+class usuario {
+    constructor(nombre) {
+        this.nombre = nombre
     }
-    
-    let usuarios = []
-    
-    if (localStorage.getItem("usuarios")) {
-        usuarios = JSON.parse(localStorage.getItem("usuarios"))
-    } else {
-        localStorage.setItem("usuarios", JSON.stringify(usuarios))
-    }
-    
-    const formulario = document.querySelector('form')
-    const input = document.querySelector('input')
-    const botonConfirmacion = document.querySelector('button')
-    
-    
-    formulario.addEventListener("submit", (e) => {
-        e.preventDefault()
-    
-        const datForm = new FormData(e.target)
-    
-        const usuarioObj = new usuario(datForm.get("nombre"))
-    
-        usuarios.push(usuarioObj)
-        localStorage.setItem("usuarios", JSON.stringify(usuarios))
-    })
+}
 
-fetch("https://thesimpsonsquoteapi.glitch.me/quotes?characters")
+let usuarios = []
+
+if (localStorage.getItem("usuarios")) {
+    usuarios = JSON.parse(localStorage.getItem("usuarios"))
+} else {
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
+}
+
+//Funcion para guardar los nombres en el localStorage
+function form(){
+
+const formulario = document.querySelector('form')
+
+formulario.addEventListener("submit", (e) => {
+
+    const datForm = new FormData(e.target)
+
+    const usuarioObj = new usuario(datForm.get("nombre"))
+
+    usuarios.push(usuarioObj)
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
+})
+}
+
+
+//Mostrando API de los simpsons en el HTML
+fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
 
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data =>{
+    const {image, quote} = data[0]
+    const divImg = document.getElementById("imgSimpson")
+    divImg.innerHTML =`
+     <div>
+         <p>${quote}</p>
+         <img class"imgSimp" src="${image}"></img>
 
+     <div>
+    `
+})
+
+//Inicializando las variables
 let tarjetasDestapadas = 0;
 let imagenUno = null;
 let imagenDos = null;
@@ -50,6 +63,7 @@ numeros = numeros.sort(() => {
 });
 console.log(numeros);
 
+//Funcion principal, para destapar las img
 function destapar(id) {
 
     tarjetasDestapadas++;
@@ -57,18 +71,22 @@ function destapar(id) {
 
     if (tarjetasDestapadas == 1) {
 
+    //Muestra la primera imagen
         imagenUno = document.getElementById(id);
 
         primerResultado = numeros[id];
         imagenUno.innerHTML = `<img src="./img/${primerResultado}.png" alt="">`;
 
+        //Deshabilita el primer boton
         imagenUno.disabled = true;
     } else if (tarjetasDestapadas == 2) {
 
+   //Muestra la segunda imagen
         imagenDos = document.getElementById(id);
         segundoResultado = numeros[id];
-        imagenDos.innerHTML = `<img src="./img/${segundoResultado}.png" alt="">`;;
+        imagenDos.innerHTML = `<img src="./img/${segundoResultado}.png" alt="">`;
 
+        //Deshabilita el segundo boton   
         imagenDos.disabled = true;
 
         if (primerResultado == segundoResultado) {
@@ -76,33 +94,53 @@ function destapar(id) {
                 text: "Excelente!",
                 className: "info",
                 style: {
-                    background: "bg-primary",
+                    background: "pink",
                 }
             }).showToast();
+
+            //Contador tarjetas destapadas
             tarjetasDestapadas = 0;
 
+            //aumenta los aciertos
             aciertos++;
+
+            //mostrando los aciertos en el HTMl
             mostrarAciertos.innerHTML = `Aciertos: ${aciertos}`
-            if (aciertos == 8) {
-                mostrarAciertos.innerHTML = `Aciertos: ${aciertos} Ganaste!`
-            }
-        } else {
-            Toastify({
-                text: "Sigue intentando",
-                className: "info",
-                style: {
-                    background: "bg-primary",
-                    padding: 20,
+             if (aciertos == 8) {
+
+                const usuarioStorage = JSON.parse(localStorage.getItem("usuarios"))
+                usuarioStorage.forEach((usuarios)=>{
+
+                mostrarAciertos.innerHTML = ` ${aciertos} Ganaste! ${usuarios}`
+                 
+                Toastify({
+                    text: "Presiona aqui si quieres volver a jugar",
+                    duration: 10000,
+                    destination: "index.html",
+                    newWindow: true,
+                    close: true,
+                    gravity: "button", 
+                    position: "center", 
+                    stopOnFocus: true, 
+                    style: {
+                      background: "pink",
+                      text:"center",
+                    },
+                    onClick: function(){}
+                  }).showToast();
+                })
                 }
-            }).showToast();
+            } else {
+             //Muestra temporalmente los resultados y sino son iguales las imagenes, las vuelve a tapar
             setTimeout(() => {
                 imagenUno.innerHTML = ' ';
                 imagenDos.innerHTML = ' ';
                 imagenUno.disabled = false;
                 imagenDos.disabled = false;
                 tarjetasDestapadas = 0;
-            }, 2000);
+            }, 1000);
         }
     }
 
 }
+
